5587e0e3c4939f40a922572a71482e8537879b19
fatiihvarol <fatihvrl38@gmail.com>
2024-09-20 10:49:36 +0300

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '../styles/Navbar.css'; // CSS dosyanızı buraya ekleyin
+import logoutIcon from '../assest/logout.png'; // Logout ikonu
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+
+const Navbar = ({ userRole }) => {
+  const navigate = useNavigate();
+  const renderLinks = () => {
+    switch (userRole) {
+      case 'Employee':
+        return (
+          <>
+            <li><a href="/my-expenses">My Expenses</a></li>
+            <li><a href="/add-expense">Add Expense</a></li>
+          </>
+        );
+      case 'Manager':
+        return (
+          <>
+            <li><a href="/pending-approvals">Pending Approvals</a></li>
+            <li><a href="/reports">Reports</a></li>
+          </>
+        );
+      case 'Accountant':
+        return (
+          <>
+            <li><a href="/financial-reports">Financial Reports</a></li>
+            <li><a href="/manage-accounts">Manage Accounts</a></li>
+          </>
+        );
+      case 'Admin':
+        return (
+          <>
+            <li><a href="/user-management">User Management</a></li>
+            <li><a href="/site-settings">Site Settings</a></li>
+          </>
+        );
+      default:
+        return <li><a href="/login">Login</a></li>;
+    }
+  };
+
+  const handleLogout = () => {
+    navigate('/login');
+    localStorage.clear();
+  };
+
+  return (
+    <nav className="navbar">
+      <ul>
+        {renderLinks()}
+      </ul>
+      <img 
+        src={logoutIcon} 
+        alt="Logout" 
+        className="logout-icon" 
+        onClick={handleLogout} 
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/screens/employee/EmployeeDashboard.js
+++ src/screens/employee/EmployeeDashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { test } from '../../services/Test';
 import ExpenseForm from './ExpenseForm';
+import Navbar from '../../components/Navbar';
 const EmployeeDashboard = () => {
 
     return (
         <div>
+          <Navbar userRole = "Employee"></Navbar>
           <ExpenseForm />
         </div>
       );
