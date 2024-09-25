3fa8c71308d773385ed4ea2c44abdb19a5fa6b38
fatiihvarol <fatihvrl38@gmail.com>
2024-09-25 17:42:57 +0300

    update

--- src/services/ExpenseFormHistoryService.js
+++ src/services/ExpenseFormHistoryService.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 import { BASEURL } from "../config/Constants";
 
+
 const HISTORYURL = `${BASEURL}/ExpenseFormHistories`;
 
 export const fetchExpenseFormHistory = async (id) => {
-    try {
-        const response = await axios.get(`${HISTORYURL}/${id}`);
-        return response.data;
-    } catch (error) {
-        throw new Error(error.response ? error.response.data.message : 'Failed to fetch history');
-    }
+  try {
+    const token = localStorage.getItem('token');
+    const response = await axios.get(`${HISTORYURL}/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response ? error.response.data.message : "Failed to fetch history"
+    );
+  }
 };
